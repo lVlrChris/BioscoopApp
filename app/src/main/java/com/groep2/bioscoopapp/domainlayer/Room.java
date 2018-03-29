6ccde4df303e26a8141f6e34db90c720b88945f9
KevinvanLoon <kloon3@avans.nl>
2018-03-29 15:05:24 +0200

    Aan een zaal een array van stoelen toegevoegd

--- app/src/main/java/com/groep2/bioscoopapp/domainlayer/Room.java
+++ app/src/main/java/com/groep2/bioscoopapp/domainlayer/Room.java
@@ -1,6 +1,8 @@
 package com.groep2.bioscoopapp.domainlayer;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kevin van Loon on 27-3-2018.
@@ -8,9 +10,11 @@ import java.io.Serializable;
 
 public class Room implements Serializable {
     private int RoomID;
+    private List<Seat> seats;
 
     public Room(int roomID) {
-        RoomID = roomID;
+        this.RoomID = roomID;
+        this.seats = createSeats();
     }
 
     public int getRoomID() {
@@ -19,6 +23,28 @@ public class Room implements Serializable {
 
     public void setRoomID(int roomID) {
         RoomID = roomID;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public ArrayList<Seat> createSeats(){
+        ArrayList seats = new ArrayList();
+        seats.add(new Seat(1));
+        seats.add(new Seat(2));
+        seats.add(new Seat(3));
+        seats.add(new Seat(4));
+        seats.add(new Seat(5));
+        seats.add(new Seat(6));
+        seats.add(new Seat(7));
+        seats.add(new Seat(8));
+
+        return seats;
     }
 
     @Override
--- app/src/main/java/com/groep2/bioscoopapp/domainlayer/Seat.java
+++ app/src/main/java/com/groep2/bioscoopapp/domainlayer/Seat.java
@@ -1,8 +1,24 @@
 package com.groep2.bioscoopapp.domainlayer;
 
+import java.io.Serializable;
+
 /**
  * Created by Kevin van Loon on 27-3-2018.
  */
 
-public class Seat {
+public class Seat implements Serializable{
+
+    private int seatID;
+
+    public Seat(int seatID) {
+        this.seatID = seatID;
+    }
+
+    public int getSeatID() {
+        return seatID;
+    }
+
+    public void setSeatID(int seatID) {
+        this.seatID = seatID;
+    }
 }
