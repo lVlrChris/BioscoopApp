7926d4f6e778001e15a52896ed113a2c134497aa
ItzJasonnn <32130586+ItzJasonnn@users.noreply.github.com>
2018-04-05 14:21:19 +0200

    Google maps intent

--- app/src/main/java/com/groep2/bioscoopapp/guilayer/CinemaActivity.java
+++ app/src/main/java/com/groep2/bioscoopapp/guilayer/CinemaActivity.java
@@ -1,7 +1,10 @@
 package com.groep2.bioscoopapp.guilayer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,6 +33,17 @@ public class CinemaActivity extends AppCompatActivity {
         mail.setText(mailStartString + cinema.getMail());
         TextView tel = findViewById(R.id.ca_phoneNumber);
         tel.setText(telStartString + cinema.getTel());
+    }
+
+    public void launchMap(View view){
+        Intent intent = null;
+
+        intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.google.nl/maps/place/Path%C3%A9+Breda/@51.5839355,4.7770626,14.74z/data=!4m5!3m4!1s0x47c69f7f704bcddd:0x39798afaa1dc4718!8m2!3d51.5897361!4d4.7850448"));
+        startActivity(intent);
+        //geo:51.5850876,4.7910311
+
+//        String address = "http://maps.google.com/maps?daddr= +
 
     }
 }
