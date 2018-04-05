25f745f05e395f8f08d8eb892c21f9607c0e714a
Kevin van Loon <34369608+KevinvanLoon@users.noreply.github.com>
2018-04-05 15:38:58 +0200

    User activity list met betaalde tickets(zonder db)

--- app/src/main/java/com/groep2/bioscoopapp/guilayer/UserActivity.java
+++ app/src/main/java/com/groep2/bioscoopapp/guilayer/UserActivity.java
@@ -2,14 +2,35 @@ package com.groep2.bioscoopapp.guilayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.groep2.bioscoopapp.R;
+import com.groep2.bioscoopapp.applicationlogic.TicketAdapter;
+import com.groep2.bioscoopapp.applicationlogic.TicketManager;
 
 public class UserActivity extends AppCompatActivity {
+
+
+    ListView tickets;
+    TicketAdapter adapter;
+    TicketManager ticketManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        ticketManager= ticketManager.getInstance(getApplicationContext());
+
+        //Zet de titel van de film
+        tickets = findViewById(R.id.au_tickets);
+        adapter = new TicketAdapter(this, ticketManager.getTickets());
+        tickets.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
+
+    public void dataChanged() {
+        adapter.notifyDataSetChanged();
+    }
+
 }
